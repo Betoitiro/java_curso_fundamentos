25be5714e36191b3e81294f6ed96292cbe0299e7
humberto itiro <itirotrabalhos@gmail.com>
2023-03-28 16:46:41 -0300

    converction in variables

---
+++ conversao_entre_variaveis.java
@@ -0,0 +1,25 @@
+package exercicios_curso_Java.java_curso;
+
+public class conversao_entre_variaveis {
+
+	public static void main(String[] args) {
+	
+		double a = 1; //conversão implicita
+		
+		float b = (float)1.0; //conversão explicita cast
+		
+		int c = 130;
+		//byte d = (byte)c; explicita
+		//nesse caso daria errado, ja que o tipo int, é superior 
+		//ao tipo byte
+		
+		double e = 1.999;
+		int f = (int) e;//conversão explicita usando cast
+		
+		
+				
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(f);
+	}
+}
