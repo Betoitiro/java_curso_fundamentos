ee53a8375687b317472cce48aeadc3b121d9e1b2
humberto itiro <itirotrabalhos@gmail.com>
2023-03-29 00:07:55 -0300

    desafio

---
+++ desafio_aritmeticos.java
@@ -0,0 +1,50 @@
+package exercicios_curso_Java.java_curso;
+
+public class desafio_aritmeticos {
+
+	
+	public static void main(String[] args) {
+		
+		//int a = 3*4 - 10;
+		//int b = (int) Math.pow(a, 3);
+		
+		//System.out.println(b);
+		
+		
+		//realizar as opeações
+		
+		
+		int a = 6;
+		int b = 3;
+		int c = 2;
+		int d = 5;
+		int e = 1;
+		int f = 7;
+		int g = 10;
+		
+		int h = (a*(b+c));		
+		int i = (int) Math.pow(h, 2);
+		int y = i/(b*c);
+		System.out.println(y);
+		
+		int j = (e-d)*(c-f);
+		int t = j/c;
+		int k = (int) Math.pow(t, 2);
+		//System.out.println(k);
+		
+		int l = y-k;
+		System.out.println(l);
+		
+		int m = (int) Math.pow(l, 3);
+		int q = (int) Math.pow(g, b);
+		int o = m/q;
+		System.out.println(o);
+		
+		//resposta 125
+		
+		
+		
+		
+		
+	}
+}
