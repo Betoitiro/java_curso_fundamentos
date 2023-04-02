2ee925fa8d78d6f8ee92cea6bd423255653f8808
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 18:33:52 -0300

    operadores logicos

--- opeadrores_logicos.java
+++ opeadrores_logicos.java
@@ -4,5 +4,29 @@ public class opeadrores_logicos {
         boolean condicao2 = 3>7;
 
         System.out.println(condicao1 && condicao2);
+        System.out.println(condicao1 || condicao2);
+        System.out.println(condicao1 ^ condicao2);
+        System.out.println(!!condicao1);
+        System.out.println(!condicao2);
+
+        System.out.println("\n tabela verdade");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+
+        System.out.println("\n tabela verdade");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+        System.out.println("\n tabela verdade");
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(false ^ false);
+
     }
 }
