eefcfff91658247a7a558252857879b734d85c4b
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 21:29:58 -0300

    condição de vedadeiro e falto com uso da tabela

--- desafio_operadores_logicos.java
+++ desafio_operadores_logicos.java
@@ -4,13 +4,20 @@ public class desafio_operadores_logicos {
         //trabalho na terça (v ou f)
         // trabalho na quinta (v ou f)
 
-        boolean trabalho = true;
-        boolean trabalho2 = true;
+        boolean trabalho = false;
+        boolean trabalho2 = false;
 
         boolean comproutv50 =  trabalho && trabalho2;
+        boolean comprouTV32 = trabalho ^trabalho2;
+        boolean comprouSorvete = trabalho || trabalho2;
+        boolean maissaudavel = !comprouSorvete;
+
+
 
         System.out.println("comprou tv 50\"?" + comproutv50);
-
+        System.out.println("comprou tv de 32\"?" + comprouTV32);
+        System.out.println("comprou tv de 32\"sorvete?" + comprouSorvete);
+        System.out.println("mais saudavel?" + !comprouSorvete);
        
 
 
