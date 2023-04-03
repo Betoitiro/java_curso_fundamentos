031af3032a5a60ddcc6616b5e25c295344ffda70
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 21:42:19 -0300

    operadores_Relacionais

---
+++ operadores/operadores_Relacionais.java
@@ -0,0 +1,16 @@
+package operadores;
+
+public class operadores_Relacionais {
+
+    public static void main(String[] args) {
+        
+        //metodo não muito utilizado, pois deixa o codio confuso
+        int a = 97;
+        int b = 'a';
+
+        System.out.println(a == b);
+
+        System.out.println('\u0061'); //o valor corresponde a 97 no hexa.
+    }
+    
+}
