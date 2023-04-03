15607e6159c4976cb080ef9f2771a02c877a662b
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 21:25:04 -0300

    desafui

---
+++ desafio_operadores_logicos.java
@@ -0,0 +1,18 @@
+public class desafio_operadores_logicos {
+    public static void main(String[] args) {
+        
+        //trabalho na terça (v ou f)
+        // trabalho na quinta (v ou f)
+
+        boolean trabalho = true;
+        boolean trabalho2 = true;
+
+        boolean comproutv50 =  trabalho && trabalho2;
+
+        System.out.println("comprou tv 50\"?" + comproutv50);
+
+       
+
+
+    }
+}
