2d9c640d2781a4f99f582219bf1e46b4d4bdeb5e
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 23:42:47 -0300

    operadores_unuarios

--- operadores/operadores_unuarios.java
+++ operadores/operadores_unuarios.java
@@ -1,12 +1,26 @@
 package operadores;
 
+import manipulação.string_numero;
+
 public class operadores_unuarios {
     
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
 
-        a++;
-        a--;
+        a++; //a = a +1
+        a--; // a = a -1
+        
+        b++; // b = b + 1
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);//teve um encremento
+        System.out.println("mine desafio...");
+        System.out.println(++a == b--);
+        System.out.println(a == b);//nesse caso sera false, devido a linnha de precedencia!
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
