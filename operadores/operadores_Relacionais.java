8b6b2f7b02497625bee56c86ec7f4e7fc254293f
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-02 21:49:37 -0300

    operadores_Relacionais

--- operadores/operadores_Relacionais.java
+++ operadores/operadores_Relacionais.java
@@ -10,7 +10,25 @@ public class operadores_Relacionais {
 
         System.out.println(a == b);
 
-        System.out.println('\u0061'); //o valor corresponde a 97 no hexa.
+        //System.out.println('\u0061'); //o valor corresponde a 97 no hexa.
+
+        //outros operadores relacionais
+        System.out.println(3 > 4);
+        System.out.println(3 < 4);
+        System.out.println(5<9);
+        System.out.println(30 <=70);
+        System.out.println(50 !=60);
+
+        //como usar os operadores relacionais no dia a dia
+        double nota = 7.3;
+        boolean bomComportamento = true;
+        boolean passouPorMedia = nota >= 7;
+
+        boolean temDesconto = bomComportamento && passouPorMedia;
+
+        System.out.println("tem desconto? "+ temDesconto);
+
+
     }
     
 }
