968b7235852542c1141d596a189b7cf06ea6e6db
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-04 00:02:31 -0300

    modificações

---
+++ manipulação/numero_string.java
@@ -0,0 +1,22 @@
+package manipulação;
+
+public class numero_string {
+	public static void main(String[] args) {
+		//conversão de numeros para tipos numericos
+		
+		Integer num = 1000;
+		
+		System.out.println(num.toString().length()); //maneira de conversão
+		
+		int num1 = 100000;
+		
+		System.out.println(Integer.toString(num1).length());//maneira de conversão
+		
+		System.out.println(("" + num1).length());//maneira de conversão
+		
+		
+		
+	
+	
+	}
+}
