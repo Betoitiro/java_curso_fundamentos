ded602f88756eee414018deec4bb2a5b451b9ba3
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-03 23:01:09 -0300

    equals

--- exercicios/exercicios/tipo_string.java
+++ exercicios/exercicios/tipo_string.java
@@ -1,7 +1,24 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class tipo_string {
+	
 	public static void main(String[] args) {
-		System.out.println("2" == "2");
+		
+		System.out.println("2" == "2"); //true
+		
+		String s = new String ("2"); 
+		System.out.println("2" == s);//false
+		System.out.println("2".equals(s)); //true
+		//.equals serve para comprar o conteudo
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		String s2 = entrada.next();
+		System.out.println("2" == s2.trim());//sem o equals dara false
+		System.out.println("2".equals(s2.trim()));//.trim serve para tirar os espaços
+		//ponto chave da aula equals, que compara o produto, e verificara a risca o tipo da variavel
+		entrada.close();
 	}
 }
