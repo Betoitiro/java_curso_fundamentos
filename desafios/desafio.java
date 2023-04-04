e0b3c27f094bfb3cb1329baf91ec51a158acb478
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-03 23:52:30 -0300

    correção de erros

---
+++ desafios/desafio.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+public class desafio {
+	public static void main(String[] args) {
+		//desafio calculadora
+		
+		Scanner entrada = new Scanner(System.in);
+	
+		//primeiro pede ao usuario que informe dois numeros
+		System.out.println("informe um numero: ");
+		double n1 = entrada.nextDouble();
+	
+		System.out.println("informe um numero: ");
+		double n2 = entrada.nextDouble();
+		
+		//pedimos para que informa a operação
+		System.out.println("informa a operação");
+		String op = entrada.next();
+	
+		//logica do programa de como funcionara as operações
+		
+		double res = "+".equals(op) ? n1+ n2 :0;
+		res = "-".equals(op) ? n1 - n2 : res;
+		res = "*".equals(op) ? n1 * n2 : res;
+		res = "/".equals(op) ? n1 / n2 : res;
+		res = "%".equals(op) ? n1 % n2 : res;
+	
+	
+	
+		System.out.printf("%.2f %s %.2f = %2.f", 
+				n1, op, n2, res);
+	
+		entrada.close();
+}
+	}
+
